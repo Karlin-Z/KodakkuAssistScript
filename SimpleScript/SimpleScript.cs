ed61bf9847d4c66abc3691018deb7171e0a49bca
Karlin <zhu337466112@gmail.com>
2024-06-26 11:22:38 +0800

    Add Comments

--- SimpleScript/SimpleScript.cs
+++ SimpleScript/SimpleScript.cs
@@ -3,24 +3,46 @@ using KodakkuAssist.Module.GameEvent;
 using KodakkuAssist.Script;
 using KodakkuAssist.Module.GameEvent.Struct;
 using KodakkuAssist.Module.Draw;
+using System.Windows.Forms;
 
 namespace MyScriptNamespace
 {
-
-    [ScriptType(name:"SimpleScript",guid: "d3b6a9b4-1e0e-4e0c-b7c7-ff1fce0e6cf2")]
+    /// <summary>
+    /// name and version affect the script name and version number displayed in the user interface.
+    /// Classes with the same GUID will be considered the same trigger. Please ensure your GUID is unique and does not conflict with others.
+    /// </summary>
+    [ScriptType(name:"SimpleScript",guid: "d3b6a9b4-1e0e-4e0c-b7c7-ff1fce0e6cf2",version:"0.0.0.1")]
     public class SimpleScript
     {
-        [UserSetting("Test Property")]
+        /// <summary>
+        /// note will be displayed to the user as a tooltip.
+        /// </summary>
+        [UserSetting(note:"This is a test Property")]
         public int prop1 { get; set; } = 1;
         [UserSetting("Another Test Property")]
         public bool prop2 { get; set; } = false;
         int n = 0;
 
+        /// <summary>
+        /// This method is called at the start of each battle reset.
+        /// If this method is not defined, the program will execute an empty method.
+        /// </summary>
         public void Init()
         {
             n = 0;
         }
 
+        /// <summary>
+        /// name is the name of the method as presented to the user.
+        /// eventType is the type of event that triggers this method.
+        /// eventCondition is an array of strings specifying the properties that the event must have,
+        /// in the format name:value,For specific details, please refer to the GameEvent of the plugin.
+        /// userControl set to false will make the method not be shown to the user
+        /// and cannot be disabled by the user.
+        /// Please note, the method will be executed asynchronously.
+        /// </summary>
+        /// <param name="event">The event instance that triggers this method.</param>
+        /// <param name="accessory">Pass the instances of methods and data that might be needed.</param>
         [ScriptMethod(name: "Test StartCasting",eventType: EventTypeEnum.StartCasting,eventCondition: ["ActionId:0085"])]
         public void PrintInfo(Event @event, ScriptAccessory accessory)
         {
