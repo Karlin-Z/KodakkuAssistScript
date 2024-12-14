a502f9388899eff5005503751e41c72b13bdd22c
Karlin <zhu337466112@gmail.com>
2024-12-14 22:21:32 +0800

    新增欧米茄

---
+++ 06-EndWalker/OmegaProtocolUltimate.cs
@@ -0,0 +1,412 @@
+﻿using System;
+using KodakkuAssist.Module.GameEvent;
+using KodakkuAssist.Script;
+using KodakkuAssist.Module.GameEvent.Struct;
+using KodakkuAssist.Module.Draw;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ECommons;
+using System.Numerics;
+using Newtonsoft.Json;
+using System.Linq;
+using System.ComponentModel;
+using System.Xml.Linq;
+using Dalamud.Utility.Numerics;
+
+namespace MyScriptNamespace
+{
+    
+    [ScriptType(name: "OmegaProtocolUltimate", territorys: [1122],guid: "625eb340-0811-4c37-b87c-c46fe5204940", version:"0.0.0.1",note: noteStr)]
+    public class OmegaProtocolUltimate
+    {
+        const string noteStr =
+        """
+        欧米茄验证绝境战
+        """;
+        List<int> HtdhParty = [2, 0, 1, 4, 5, 6, 7, 3];
+        double parse = 0;
+
+        uint P1_BossId = 0;
+        List<int> P1_点名Buff = [0, 0, 0, 0, 0, 0, 0, 0];
+        List<Vector3> P1_TowerPos = [];
+        DateTime P1_TowerTime= DateTime.MinValue;
+        DateTime P1_FanTime = DateTime.MinValue;
+        int P1_LineRound = 0;
+        int P1_FireCount = 0;
+
+        public void Init(ScriptAccessory accessory)
+        {
+            parse = 0;
+        }
+        #region P1
+        [ScriptMethod(name: "P1_循环程序_分P", eventType: EventTypeEnum.StartCasting, eventCondition: ["ActionId:31491"],userControl:false)]
+        public void P1_循环程序_分P(Event @event, ScriptAccessory accessory)
+        {
+            if (!ParseObjectId(@event["TargetId"], out var tid)) return;
+            P1_BossId=tid;
+            parse = 1.1;
+            P1_TowerPos = [];
+            P1_LineRound = 0;
+        }
+        [ScriptMethod(name: "P1_循环程序_Buff记录", eventType: EventTypeEnum.StatusAdd, eventCondition: ["StatusID:regex:^(3004|3005|3006|3451)$"],userControl:false)]
+        public void P1_循环程序_Buff记录(Event @event, ScriptAccessory accessory)
+        {
+            if (parse != 1.1) return;
+            if (!ParseObjectId(@event["TargetId"], out var tid)) return;
+            var index = accessory.Data.PartyList.IndexOf(tid);
+            P1_点名Buff[index] = @event["StatusID"] switch
+            {
+                "3004" => 1,
+                "3005" => 2,
+                "3006" => 3,
+                "3451" => 4,
+                _=>0
+            };
+        }
+        [ScriptMethod(name: "P1_循环程序_塔收集", eventType: EventTypeEnum.ObjectChanged, eventCondition: ["Operate:Add", "DataId:2013245"], userControl: false)]
+        public void P1_循环程序_塔收集(Event @event, ScriptAccessory accessory)
+        {
+            var pos = JsonConvert.DeserializeObject<Vector3>(@event["SourcePosition"]);
+            lock (P1_TowerPos)
+            {
+                P1_TowerPos.Add(pos);
+            }
+        }
+        [ScriptMethod(name: "P1_循环程序_集合提醒", eventType: EventTypeEnum.StartCasting, eventCondition: ["ActionId:31491"])]
+        public void P1_循环程序_集合提醒(Event @event, ScriptAccessory accessory)
+        {
+            if (parse != 1.1) return;
+            accessory.Method.TextInfo("Boss背后集合", 2000);
+            accessory.Method.TTS("Boss背后集合");
+        }
+        [ScriptMethod(name: "P1_循环程序_开始站位提醒", eventType: EventTypeEnum.StatusAdd, eventCondition: ["StatusID:regex:^(3004|3005|3006|3451)$"])]
+        public void P1_循环程序_开始站位提醒(Event @event, ScriptAccessory accessory)
+        {
+            if (parse != 1.1) return;
+            if (!ParseObjectId(@event["TargetId"], out var tid)) return;
+            if (tid != accessory.Data.Me) return;
+            if (@event["StatusID"]=="3006")
+            {
+                accessory.Method.TextInfo("靠前接线", 3000);
+                accessory.Method.TTS("靠前接线");
+            }
+            else
+            {
+                accessory.Method.TextInfo("靠后", 3000);
+                accessory.Method.TTS("靠后");
+            }
+           
+        }
+        [ScriptMethod(name: "P1_循环程序_线塔处理位置", eventType: EventTypeEnum.ObjectChanged, eventCondition: ["Operate:Add", "DataId:2013245"])]
+        public async void P1_循环程序_线塔处理位置(Event @event, ScriptAccessory accessory)
+        {
+            if (parse != 1.1) return;
+            lock (this)
+            {
+                if ((DateTime.Now - P1_TowerTime).TotalSeconds < 2) return;
+                P1_TowerTime=DateTime.Now;
+            }
+            await Task.Delay(50);
+            Vector3 centre = new(100, 0, 100);
+            var towerCount = P1_TowerPos.Count;
+            List<int> HtdhParty = [2, 0, 1, 4, 5, 6, 7, 3];
+            var myindex=accessory.Data.PartyList.IndexOf(accessory.Data.Me);
+            var myBuff = P1_点名Buff[myindex];
+            var index1 = P1_点名Buff.IndexOf(myBuff);
+            var index2 = P1_点名Buff.LastIndexOf(myBuff);
+            var hIndex = HtdhParty.IndexOf(index1) < HtdhParty.IndexOf(index2) ? index1 : index2;
+            var meIsHigh = hIndex == myindex;
+            var idle = false;
+            //塔
+            if (towerCount == myBuff * 2)
+            {
+                idle=true;
+                var hPos=default(Vector3);
+                var lPos=default(Vector3);
+                if (PositionTo4Dir(P1_TowerPos[towerCount - 2], centre) < PositionTo4Dir(P1_TowerPos[towerCount - 1], centre))
+                {
+                    hPos = P1_TowerPos[towerCount - 2];
+                    lPos = P1_TowerPos[towerCount - 1];
+                }
+                else
+                {
+                    hPos = P1_TowerPos[towerCount - 1];
+                    lPos = P1_TowerPos[towerCount - 2];
+                }
+                var dealpos = meIsHigh?hPos:lPos;
+
+                var dp = accessory.Data.GetDefaultDrawProperties();
+                dp.Name = "P1_循环程序_塔站位";
+                dp.Scale = new(2);
+                dp.Owner = accessory.Data.Me;
+                dp.TargetPosition = dealpos;
+                dp.ScaleMode |= ScaleMode.YByDistance;
+                dp.Color = accessory.Data.DefaultSafeColor;
+                dp.DestoryAt = 9000;
+                accessory.Method.SendDraw(DrawModeEnum.Imgui, DrawTypeEnum.Displacement, dp);
+
+                dp = accessory.Data.GetDefaultDrawProperties();
+                dp.Name = "P1_循环程序_塔范围";
+                dp.Scale = new(3);
+                dp.Position = dealpos;
+                dp.Color = accessory.Data.DefaultSafeColor;
+                dp.DestoryAt = 9000;
+                accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Circle, dp);
+            }
+            //线
+            if (towerCount == (myBuff + 2) * 2 % 8)
+            {
+                idle = true;
+                List<int> isTower = [0, 0, 0, 0];
+                isTower[PositionTo4Dir(P1_TowerPos[towerCount - 2], centre)] = 1;
+                isTower[PositionTo4Dir(P1_TowerPos[towerCount - 1], centre)] = 1;
+                var my4Dir = meIsHigh ? isTower.IndexOf(0) : isTower.LastIndexOf(0);
+                var dealpos = RotatePoint(new(100, 0, 85), centre, float.Pi / 2 * my4Dir);
+
+                var dp = accessory.Data.GetDefaultDrawProperties();
+                dp.Name = "P1_循环程序_线站位";
+                dp.Scale = new(2);
+                dp.Owner = accessory.Data.Me;
+                dp.TargetPosition = dealpos;
+                dp.ScaleMode |= ScaleMode.YByDistance;
+                dp.Color = accessory.Data.DefaultSafeColor;
+                dp.DestoryAt = 9000;
+                accessory.Method.SendDraw(DrawModeEnum.Imgui, DrawTypeEnum.Displacement, dp);
+            }
+            //闲
+            if (!idle)
+            {
+                //北点 100,0,86
+                var myPos = accessory.Data.Objects.SearchByEntityId(accessory.Data.Me)?.Position??default;
+                var drot = (myPos - P1_TowerPos[towerCount - 2]).Length() < (myPos - P1_TowerPos[towerCount - 1]).Length() ? PositionTo4Dir(P1_TowerPos[towerCount - 2],centre) : PositionTo4Dir(P1_TowerPos[towerCount - 1], centre);
+                var dealpos=RotatePoint(new(100,0,86),centre,float.Pi/2*drot);
+                var dp = accessory.Data.GetDefaultDrawProperties();
+                dp.Name = "P1_循环程序_闲站位";
+                dp.Scale = new(2);
+                dp.Owner = accessory.Data.Me;
+                dp.TargetPosition = dealpos;
+                dp.ScaleMode |= ScaleMode.YByDistance;
+                dp.Color = accessory.Data.DefaultSafeColor;
+                dp.DestoryAt = 9000;
+                accessory.Method.SendDraw(DrawModeEnum.Imgui, DrawTypeEnum.Displacement, dp);
+            }
+        }
+        [ScriptMethod(name: "P1_循环程序_接线标记", eventType: EventTypeEnum.ActionEffect, eventCondition: ["ActionId:31496", "TargetIndex:1"])]
+        public void P1_循环程序_接线标记(Event @event, ScriptAccessory accessory)
+        {
+            if (parse != 1.1) return;
+            P1_LineRound++;
+            if (!ParseObjectId(@event["TargetId"], out var tid)) return;
+            var waitBuff = (P1_LineRound + 1) % 4 + 1;
+            var catchBuff = (P1_LineRound + 2) % 4 + 1;
+            var myindex=accessory.Data.PartyList.IndexOf(accessory.Data.Me);
+            if (catchBuff != P1_点名Buff[myindex]) return;
+
+            
+            var myBuff = P1_点名Buff[myindex];
+            var index1 = P1_点名Buff.IndexOf(myBuff);
+            var index2 = P1_点名Buff.LastIndexOf(myBuff);
+            var hIndex = HtdhParty.IndexOf(index1) < HtdhParty.IndexOf(index2) ? index1 : index2;
+            var meIsHigh = hIndex == myindex;
+
+            var index3 = P1_点名Buff.IndexOf(waitBuff);
+            var index4 = P1_点名Buff.LastIndexOf(waitBuff);
+            var hWaitIndex = HtdhParty.IndexOf(index3) < HtdhParty.IndexOf(index4) ? index3 : index4;
+            var lWaitIndex = HtdhParty.IndexOf(index3) < HtdhParty.IndexOf(index4) ? index4 : index3;
+
+            var dp = accessory.Data.GetDefaultDrawProperties();
+            dp.Name = "P1_循环程序_接线标记";
+            dp.Scale = new(10);
+            dp.Owner = tid;
+            dp.TargetObject = meIsHigh? accessory.Data.PartyList[hWaitIndex]: accessory.Data.PartyList[lWaitIndex];
+            dp.ScaleMode |= ScaleMode.YByDistance;
+            dp.Color = new(1,1,0,1);
+            dp.DestoryAt = 9000;
+            accessory.Method.SendDraw(DrawModeEnum.Imgui, DrawTypeEnum.Line, dp);
+        }
+        [ScriptMethod(name: "P1_循环程序_接线标记移除", eventType: EventTypeEnum.Tether, eventCondition: ["Id:0059"])]
+        public void P1_循环程序_接线标记移除(Event @event, ScriptAccessory accessory)
+        {
+            if (parse != 1.1) return;
+            if (!ParseObjectId(@event["SourceId"], out var sid)) return;
+            if (sid != accessory.Data.Me) return;
+            accessory.Method.RemoveDraw("P1_循环程序_接线标记");
+        }
+
+        [ScriptMethod(name: "P1_全能之主_分P", eventType: EventTypeEnum.StartCasting, eventCondition: ["ActionId:31499"], userControl: false)]
+        public void P1_全能之主_分P(Event @event, ScriptAccessory accessory)
+        {
+            parse = 1.2;
+            P1_FireCount = 0;
+        }
+        [ScriptMethod(name: "P1_全能之主_Buff记录", eventType: EventTypeEnum.StatusAdd, eventCondition: ["StatusID:regex:^(3004|3005|3006|3451)$"], userControl: false)]
+        public void P1_全能之主_Buff记录(Event @event, ScriptAccessory accessory)
+        {
+            if (parse != 1.2) return;
+            if (!ParseObjectId(@event["TargetId"], out var tid)) return;
+            var index = accessory.Data.PartyList.IndexOf(tid);
+            P1_点名Buff[index] = @event["StatusID"] switch
+            {
+                "3004" => 1,
+                "3005" => 2,
+                "3006" => 3,
+                "3451" => 4,
+                _ => 0
+            };
+        }
+        [ScriptMethod(name: "P1_全能之主_高低顺位播报", eventType: EventTypeEnum.ActionEffect, eventCondition: ["ActionId:31499"])]
+        public async void P1_全能之主_高低顺位播报(Event @event, ScriptAccessory accessory)
+        {
+            if (parse != 1.2) return;
+            await Task.Delay(100);
+            var myindex=accessory.Data.PartyList.IndexOf(accessory.Data.Me);
+            var mybuff = P1_点名Buff[myindex];
+            var i1 = P1_点名Buff.IndexOf(mybuff);
+            var i2 = P1_点名Buff.LastIndexOf(mybuff);
+            var hIndex = HtdhParty.IndexOf(i1) < HtdhParty.IndexOf(i2) ? i1 : i2;
+            if (hIndex== myindex)
+            {
+                accessory.Method.TextInfo("高顺位(上右)", 10000);
+                accessory.Method.TTS("高顺位(上右)");
+            }
+            else
+            {
+                accessory.Method.TextInfo("低顺位(下左)", 10000);
+                accessory.Method.TTS("低顺位(下左)");
+            }
+        }
+        [ScriptMethod(name: "P1_全能之主_单点命中播报", eventType: EventTypeEnum.ActionEffect, eventCondition: ["ActionId:31502"])]
+        public async void P1_全能之主_单点命中播报(Event @event, ScriptAccessory accessory)
+        {
+            if (parse != 1.2) return;
+            if (!ParseObjectId(@event["TargetId"], out var tid)) return;
+            if (tid == accessory.Data.Me) 
+            {
+                accessory.Method.TextInfo("回头", 2000);
+                accessory.Method.TTS("回头");
+            }
+        }
+        [ScriptMethod(name: "P1_全能之主_分摊范围", eventType: EventTypeEnum.StatusAdd, eventCondition: ["StatusID:regex:^(350[789]|3510)$"])]
+        public void P1_全能之主_分摊范围(Event @event, ScriptAccessory accessory)
+        {
+            if (parse != 1.2) return;
+            
+            if (!ParseObjectId(@event["TargetId"], out var tid)) return;
+            if (!int.TryParse(@event["DurationMilliseconds"], out var dur)) return;
+            var dp = accessory.Data.GetDefaultDrawProperties();
+            dp.Name = "P1_全能之主_分摊范围";
+            dp.Scale = new(6,30);
+            dp.Owner = P1_BossId;
+            dp.TargetObject = tid;
+            dp.Color = accessory.Data.DefaultSafeColor;
+            dp.Delay = dur - 3000;
+            dp.DestoryAt = 3000;
+            accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Rect, dp);
+        }
+        [ScriptMethod(name: "P1_全能之主_最远距离顺劈", eventType: EventTypeEnum.TargetIcon, eventCondition: ["Id:0017"])]
+        public void P1_全能之主_最远距离顺劈(Event @event, ScriptAccessory accessory)
+        {
+            if (parse != 1.2) return;
+            lock (this)
+            {
+                if ((DateTime.Now - P1_FanTime).TotalSeconds < 20) return;
+                P1_FanTime = DateTime.Now;
+            }
+            
+            var dp = accessory.Data.GetDefaultDrawProperties();
+            dp.Name = "P1_全能之主_最远距离顺劈1";
+            dp.Scale = new(20);
+            dp.Radian = float.Pi / 3 * 2;
+            dp.Owner = P1_BossId;
+            dp.TargetResolvePattern=PositionResolvePatternEnum.PlayerFarestOrder;
+            dp.TargetOrderIndex = 1;
+            dp.Color = accessory.Data.DefaultDangerColor;
+            dp.DestoryAt = 13000;
+            accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Fan, dp);
+
+            dp = accessory.Data.GetDefaultDrawProperties();
+            dp.Name = "P1_全能之主_最远距离顺劈2";
+            dp.Scale = new(20);
+            dp.Radian = float.Pi / 3 * 2;
+            dp.Owner = P1_BossId;
+            dp.TargetResolvePattern = PositionResolvePatternEnum.PlayerFarestOrder;
+            dp.TargetOrderIndex = 2;
+            dp.Color = accessory.Data.DefaultDangerColor;
+            dp.DestoryAt = 13000;
+            accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Fan, dp);
+        }
+        [ScriptMethod(name: "P1_全能之主_点名直线", eventType: EventTypeEnum.TargetIcon, eventCondition: ["Id:0017"])]
+        public void P1_全能之主_点名直线(Event @event, ScriptAccessory accessory)
+        {
+            if (parse != 1.2) return;
+            if (!ParseObjectId(@event["TargetId"], out var tid)) return;
+            var dp = accessory.Data.GetDefaultDrawProperties();
+            dp.Name = "P1_全能之主_点名直线";
+            dp.Scale = new(6,50);
+            dp.TargetObject = tid;
+            dp.Owner = P1_BossId;
+            dp.Color = accessory.Data.DefaultDangerColor;
+            dp.DestoryAt = 5000;
+            accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Rect, dp);
+        }
+        [ScriptMethod(name: "P1_全能之主_T引导顺劈位置", eventType: EventTypeEnum.StartCasting, eventCondition: ["ActionId:32368"])]
+        public void P1_全能之主_T引导顺劈位置(Event @event, ScriptAccessory accessory)
+        {
+            lock (this)
+            {
+                P1_FireCount++;
+                if (P1_FireCount != 26) return;
+                var myindex = accessory.Data.PartyList.IndexOf(accessory.Data.Me);
+                if (myindex != 0 && myindex != 1) return;
+
+                var dp = accessory.Data.GetDefaultDrawProperties();
+                dp.Name = "P1_全能之主_T引导顺劈位置";
+                dp.Scale = new(2);
+                dp.Owner = accessory.Data.Me;
+                dp.TargetPosition = new(100,0,86);
+                dp.ScaleMode |= ScaleMode.YByDistance;
+                dp.Color = accessory.Data.DefaultSafeColor;
+                dp.DestoryAt = 11000;
+                accessory.Method.SendDraw(DrawModeEnum.Imgui, DrawTypeEnum.Displacement, dp);
+            }
+        }
+        #endregion
+
+        #region P2
+
+        #endregion
+
+
+        private static bool ParseObjectId(string? idStr, out uint id)
+        {
+            id = 0;
+            if (string.IsNullOrEmpty(idStr)) return false;
+            try
+            {
+                var idStr2 = idStr.Replace("0x", "");
+                id = uint.Parse(idStr2, System.Globalization.NumberStyles.HexNumber);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        private int PositionTo4Dir(Vector3 point, Vector3 centre)
+        {
+            var r = Math.Round(2 - 2 * Math.Atan2(point.X - centre.X, point.Z - centre.Z) / Math.PI) % 4;
+            return (int)r;
+        }
+        private Vector3 RotatePoint(Vector3 point, Vector3 centre, float radian)
+        {
+
+            Vector2 v2 = new(point.X - centre.X, point.Z - centre.Z);
+
+            var rot = (MathF.PI - MathF.Atan2(v2.X, v2.Y) + radian);
+            var lenth = v2.Length();
+            return new(centre.X + MathF.Sin(rot) * lenth, centre.Y, centre.Z - MathF.Cos(rot) * lenth);
+        }
+    }
+}
+
